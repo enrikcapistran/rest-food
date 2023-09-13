e6197c46bf2b91033c3b107702cfc9792534c3fb
Enrique Lopez Capistran <enric.capistran@gmail.com>
2023-09-13 05:09:32 -0700

    Hero Section Desktop Styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,105 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import hero from "../assets/hero.jpg";
+import heroDesign from "../assets/HeroDesign.png";
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <Section id='home'>
+        <div className="background">
+            <img src={hero} alt="Background" />
+        </div>
+        <div className="content">
+            <div className="sale">
+                <img src={heroDesign} alt="Hero Design" />
+                <h1>
+                    BIG SALE <span>50% OFF</span>
+                </h1>
+            </div>
+            <div className="info">
+                <h2>RETAILER</h2>
+                <em>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, provident.</em>
+                <button>ORDER NOW</button>
+            </div>
+        </div>
+    </Section>
   )
 }
+
+const Section = styled.section`
+  height: 90vh;
+  width: 100vw;
+  position: relative;
+  .background {
+    height: 100%;
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+      filter: brightness(60%);
+    }
+  }
+  .content {
+    position: absolute;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    width: 100%;
+    .sale {
+      position: relative;
+      left: 10%;
+      img {
+        height: 70vh;
+      }
+      h1 {
+        color: white;
+        position: absolute;
+        top: 25vh;
+        left: 15vh;
+        font-size: 4.5rem;
+        span {
+          display: block;
+          font-size: 5vw;
+        }
+      }
+    }
+    .info{
+        position: absolute;
+        top: 40%;
+        right: 10%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 1rem;
+        h2{
+            color: #f9c74f;
+            font-size: 4rem;
+            letter-spacing: 0.5rem;
+        }
+        em{
+            color: white;
+            width: 60%;
+            text-align: end;
+            font-size: 1.1rem;
+            letter-spacing: 0.1rem;
+        }
+        button{
+            padding: 1rem 2rem;
+            font-size: 1.4rem;
+            background-color: #fc4958;
+            border: none;
+            color: white;
+            font-weight: 800;
+            letter-spacing: 0.2rem;
+            cursor: pointer;
+            transition: 0ms.3s ease-in-out;
+            &:hover{
+                background-color: #f9c74f;
+            }
+        }
+        
+    }
+  }
+`;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,4 +33,54 @@ export default function Navbar() {
   )
 }
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 4vw;
+  .brand {
+    img {
+      margin-top: 1rem;
+      cursor: pointer;
+    }
+    .toggle {
+      display: none;
+    }
+  }
+  .links {
+    display: flex;
+    list-style-type: none;
+    gap: 2rem;
+    li {
+      a {
+        color: #fc4958;
+        font-weight: 600;
+        text-decoration: none;
+        text-transform: uppercase;
+        letter-spacing: 0.2rem;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #f9c74f;
+        }
+      }
+      .active {
+        color: #f9c74f;
+      }
+    }
+  }
+  @media screen and (min-width: 260px) and (max-width: 1080px) {
+    .brand {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      top: 0;
+      .toggle {
+        display: block;
+      }
+    }
+    .links {
+      display: none;
+    }
+  }
+`;
